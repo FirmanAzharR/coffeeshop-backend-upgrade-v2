763397d061a2568b7769e70473d1dea7daf71e4f
unknown <firmanazharr@gmail.com>
2022-02-18 07:17:48 +0700

    fix .env windows

--- src/config/config.js
+++ src/config/config.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 //====================================SERVER DB LOCAL===============================================//
 module.exports = {
     HOST: process.env.HOST,
-    USER: process.env.USERNAME,
+    USER: process.env.USER,
     PASSWORD: process.env.PASSWORD,
     DB: process.env.DB,
     dialect: process.env.DIALECT,
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+}
