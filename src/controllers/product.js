612d6a5a297acb665fa358c5959601103ce370e9
firman <firmanazharrnutech@gmail.com>
2022-06-27 12:34:11 +0700

    add product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,16 +1,100 @@
 const helper = require('../helper/helper')
-const { producthSchema } = require('../helper/validation')
+const {
+    producthSchema,
+    selectProducthSchema,
+    getProducts,
+} = require('../helper/validation')
 const { logs } = require('../helper/loggerMessage')
 const { CustomError } = require('../middleware/errorHandler')
 const { Op } = require('sequelize')
 const moment = require('moment')
 const db = require('../models')
-const fs = require('fs');
+const fs = require('fs')
 const fsPromises = require('fs').promises
 const modelProduct = db.product
 
 module.exports = {
-    getProduct: async () => {},
+    selectProduct: async (req, res, next) => {
+        try {
+            const { product_id } = req.body
+            await selectProducthSchema.validateAsync(req.body)
+            let result = await modelProduct.findByPk(product_id, {
+                attributes: {
+                    exclude: ['createdAt', 'deletedAt', 'updatedAt'],
+                },
+            })
+            return helper.response(res, 200, 'success select product', result)
+        } catch (e) {
+            console.log(e)
+            let message = 'Bad Request'
+            let status = 400
+            if (e.isJoi === true) {
+                message = e.details[0].message
+                status = 422
+            }
+            logs(`failed select product`, req.url, req.body, res.statusCode, {})
+            helper.response(res, status, message, {})
+            return next(new CustomError(message, 500))
+        }
+    },
+    getProduct: async (req, res, next) => {
+        try {
+            const { product_name, category_id, sortCost } = req.body
+            let { offset, limit } = req.body
+            await getProducts.validateAsync(req.body)
+            product_name != '' ? (offset = 0) : offset
+            category_id ? (offset = 0) : page
+            let result = await modelProduct.findAndCountAll({
+                where: {
+                    [Op.and]: [
+                        {
+                            product_name: {
+                                [Op.iLike]: `%${product_name}%`,
+                            },
+                        },
+                        {
+                            category_id: category_id,
+                        },
+                    ],
+                },
+                // order: [
+                //     ['id', 'DESC']
+                // ],
+                offset: offset,
+                limit: limit,
+            })
+
+            let totalPage = 0
+            if (product_name != '') {
+                totalPage = Math.ceil(result.rows.length / limit)
+            } else {
+                totalPage = Math.ceil(result.count / limit)
+            }
+
+            const pagination = {
+                totalData: result.count,
+                totalPage: totalPage,
+            }
+
+            return helper.response(
+                res,
+                200,
+                'success get all product',
+                result,
+                pagination
+            )
+        } catch (e) {
+            console.log(e)
+            let message = 'Bad Request'
+            let status = 400
+            if (e.isJoi === true) {
+                message = e.details[0].message
+                status = 422
+            }
+            helper.response(res, status, message, {})
+            return next(new CustomError(message, status))
+        }
+    },
     addProduct: async (req, res, next) => {
         try {
             const raw = req.body
@@ -28,19 +112,27 @@ module.exports = {
                 product_name: raw.product_name,
                 image: fileName,
                 description: raw.description,
+                price: raw.price,
+                discount: raw.discount,
                 active_status: raw.active_status,
             }
 
             let check = await modelProduct.findOne({
                 where: {
-                    product_name: { [Op.like] : `%${raw.product_name.toLowerCase()}%`},
+                    product_name: {
+                        [Op.like]: `%${raw.product_name.toLowerCase()}%`,
+                    },
                 },
             })
 
             if (!check) {
-                await fsPromises.writeFile(directory.local, raw.image, async (err) => {
-                    console.log(err)
-                })                
+                await fsPromises.writeFile(
+                    directory.local,
+                    raw.image,
+                    async (err) => {
+                        console.log(err)
+                    }
+                )
                 let product = await modelProduct.create(data)
                 return helper.response(res, 200, 'success add product', product)
             } else {
@@ -54,8 +146,8 @@ module.exports = {
                 message = e.details[0].message
                 status = 422
             }
-            logs(`failed add product`, req.url, {}, res.statusCode, {})
-            helper.response(res, status, 'failed add product', {})
+            logs(message, req.url, {}, res.statusCode, {})
+            helper.response(res, status, message, {})
             return next(new CustomError(message, 500))
         }
     },
